797344b6ff840df1800e8348cdf51a842c5ca369
Katsutoshi Yotsuyangi <kyotsuyanagi@cybermatrix.co>
2022-12-04 16:00:15 +0900

    update

--- sitl/sample1.py
+++ sitl/sample1.py
@@ -1,43 +1,70 @@
-#!usr/bin/env python
-# -*- coding: utf-8 -*-
-print( "SITLスタート" )    # 開始メッセージ
+from subprocess import Popen
+from signal import signal, SIGINT
+from dronekit import connect
+import time
 
-# 必要なライブラリをインポート
-import dronekit_sitl            # シミュレータをインポート
-from dronekit import connect    # フライトコントローラやシミュレータへ接続するのがdronekit内にあるconnect
-import time                     # ウェイト関数time.sleepを使うために必要
 
-# SITLの起動
-sitl = dronekit_sitl.start_default()            # sitlをデフォルト設定で起動
-connection_string = sitl.connection_string()    # 起動したsitlから，接続設定用の文字列を入手する
+print( "Starting SITL by process" )
 
-# フライトコントローラ(FC)へ接続
-# 実機ドローンだとconnection_stringは"/dev/ttyUSB0,57600"の様になる
-print( "FCへ接続: %s" % (connection_string) )    # 接続設定文字列を表示
-vehicle = connect(connection_string, wait_ready=True)    # 接続
+# example: 'dronekit-sitl copter --home=35.079624,136.905453,50.0,3.0 --instance 0'
+sitl_frame          = 'copter'          # rover, plane, copterなどのビークルタイプ
+sitl_home_latitude  = 35.879143       # 緯度(度)
+sitl_home_longitude = 140.339577      # 経度(度)
+sitl_home_altitude  = '20.0'             # 高度(m)
+sitl_home_direction = '0.0'             # 機首方位(度)
+sitl_instance_num   = 0                 # 0〜
 
-#ctrl+cが押されるまでループ
+
+total_instance = 4
+connection_instance = []
+processes = []
+
+for i in range(total_instance):
+    if i==total_instance-1:
+        sitl_home_longitude = sitl_home_longitude + 0.001000
+    else:
+        sitl_home_longitude = sitl_home_longitude + 0.000200
+
+    sitl_boot_list = ['dronekit-sitl',sitl_frame,
+                    '--home=%s,%s,%s,%s' % (str(sitl_home_latitude),str(sitl_home_longitude),sitl_home_altitude,sitl_home_direction),
+                    '--instance=%s'%(i)]
+
+    print("# sitl command:{0}".format(sitl_boot_list))
+    p = Popen(sitl_boot_list)
+    processes.append(p)
+    time.sleep(1)
+
+    '''
+    connection_string = 'tcp:localhost:' + str(5760 + int(i) * 10 )
+    print( "FC: %s" % (connection_string) )
+    connection_instance.append(connect(connection_string, wait_ready=True))
+    '''
+
 try:
-    while True:
-        # vehicleオブジェクト内のステータスを表示
-        print("--------------------------" )
-        print(" GPS: %s" % vehicle.gps_0 )
-        print(" Battery: %s" % vehicle.battery )
-        print(" Last Heartbeat: %s" % vehicle.last_heartbeat )
-        print(" Is Armable?: %s" % vehicle.is_armable )
-        print(" System status: %s" % vehicle.system_status.state )
-        print(" Mode: %s" % vehicle.mode.name )
 
+    while True:
+        print("Looping")
         time.sleep(1)
 
-except( KeyboardInterrupt, SystemExit):    # ctrl+cが押されたら離脱
-    print( "SIGINTを検知" )
+        '''
+        for i,c in enumerate(connection_instance):
+            print("Instance-"+str(i))
+            print("Mode:{0}".format(c.mode.name ))
+            time.sleep(1)
+        '''
 
-# フライトコントローラとの接続を閉じる
-vehicle.close()
+except( KeyboardInterrupt, SystemExit):
+    print( "SIGINT Detected" )
 
-# SITLを終了させる
-sitl.stop()
+'''
+for i,c in enumerate(connection_instance):
+    print("Closing instance {0}".format(i) )
+    c.close()
+'''
 
+for p in processes:
+    p.send_signal(SIGINT)
+    p.communicate()
+    time.sleep(1)
 
-print("終了．")    # 終了メッセージ
+print("All shutdown")

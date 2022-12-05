a48eb189a52216ce50048cb65859513d532002de
Katsutoshi Yotsuyanagi <kyotsuyanagi@cybermatrix.co>
2022-12-06 01:07:57 +0900

    update

--- lua/recever.lua
+++ lua/recever.lua
@@ -1,11 +1,11 @@
-local target_vel = Vector3f()
 function update () 
-  flight_mode = vehicle:get_mode()
-  if flight_mode = "HOGEHOGE" then
-    target_vel:y(2) #or target_vel:x(2)
-    gcs:send_text(0, "HOGEHOGE")
+    flight_mode = vehicle:get_mode()
+    if flight_mode == "HOGEHOGE" then
+      target_vel = Vector3f()
+      target_vel:y(2)
+      gcs:send_text(0, "HOGEHOGE")
+    end
+    gcs:send_text(0, "LUA Monitoring")
+    return update, 1000
   end
-  gcs:send_text(0, "LUA Monitoring")
   return update, 1000
-end
-return update, 1000

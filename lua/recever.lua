d4632a28b87d22962d2cb653f9b8082f9df93e2c
Katsutoshi Yotsuyanagi <kyotsuyanagi@cybermatrix.co>
2022-12-14 01:41:45 +0900

    update

--- lua/recever.lua
+++ lua/recever.lua
@@ -1,60 +1,20 @@
-already_set = false
-WAYPOINT = 16
-function avoid()
-  local current = ahrs:get_position()
-  local item = mavlink_mission_item_int_t()
-  mission:clear()
-  item:command(WAYPOINT)
-  item:x(current:lat())
-  item:y(current:lng())
-  item:z(current:alt()+10)
-
-  if mission:set_item(0, item) then
-    gcs:send_text(4, "avoiding mission is set")
-    mission:start(0)
-    gcs:send_text(4, "avoiding mission is executed")
-    return true
-  else
-    gcs:send_text(4, "avoiding mission is failed to set")
-    return false
-  end
-end
-
-function return_orginal()
-  local current = ahrs:get_position()
-  local item = mavlink_mission_item_int_t()
-  mission:clear()
-  item:command(WAYPOINT)
-  item:x(current:lat())
-  item:y(current:lng())
-  item:z(current:alt()-10)
-  
-  if mission:set_item(0, item) then
-    gcs:send_text(4, "returning mission is set")
-    mission:start(0)
-    gcs:send_text(4, "returning mission is executed")
-    return true
-  else
-    gcs:send_text(4, "returning mission is failed to set")
-    return false
-  end
-end
-
+already_set == false
 
 function update () 
   flight_mode = vehicle:get_mode()
   if flight_mode == 111 and already_set == false then
     gcs:send_text(0, "HOGEHOGE")
-    if avoid() then
-      gcs:send_text(4, "Avoiding")
-      already_set = true
-    end
+    local target_vel = Vector3f()
+    target_vel:x(2)
+    vehicle:set_target_velocity_NED(target_vel))
+    already_set = true
   end
   if flight_mode == 4 and already_set == true then
-    if return_orginal() then
-      already_set = false
-      gcs:send_text(0, "Rest Monitoring")
-    end
+    already_set = false
+    gcs:send_text(0, "Rest Monitoring")
+    local target_vel = Vector3f()
+    target_vel:x(-2)
+    vehicle:set_target_velocity_NED(target_vel))
   end
   return update, 1000
 end

a607db6368298b498b89ac91ee1ef8dfe06eb461
Katsutoshi Yotsuyangi <kyotsuyanagi@cybermatrix.co>
2022-12-14 01:36:49 +0900

    update

--- lua/recever.lua
+++ lua/recever.lua
@@ -1,19 +1,23 @@
 already_set = false
-
+WAYPOINT = 16
 function avoid()
   local current = ahrs:get_position()
   local item = mavlink_mission_item_int_t()
   mission:clear()
   item:command(WAYPOINT)
-  item:x(current:lat() + 0.001)
+  item:x(current:lat())
   item:y(current:lng())
-  item:z(current:alt())
-  
-  if not mission:set_item(0, item) then
+  item:z(current:alt()+10)
+
+  if mission:set_item(0, item) then
     gcs:send_text(4, "avoiding mission is set")
+    mission:start(0)
+    gcs:send_text(4, "avoiding mission is executed")
+    return true
+  else
+    gcs:send_text(4, "avoiding mission is failed to set")
     return false
   end
-  return true
 end
 
 function return_orginal()
@@ -21,15 +25,19 @@ function return_orginal()
   local item = mavlink_mission_item_int_t()
   mission:clear()
   item:command(WAYPOINT)
-  item:x(current:lat() - 0.001)
+  item:x(current:lat())
   item:y(current:lng())
-  item:z(current:alt())
+  item:z(current:alt()-10)
   
-  if not mission:set_item(0, item) then
+  if mission:set_item(0, item) then
     gcs:send_text(4, "returning mission is set")
+    mission:start(0)
+    gcs:send_text(4, "returning mission is executed")
+    return true
+  else
+    gcs:send_text(4, "returning mission is failed to set")
     return false
   end
-  return true
 end
 
 
@@ -37,11 +45,16 @@ function update ()
   flight_mode = vehicle:get_mode()
   if flight_mode == 111 and already_set == false then
     gcs:send_text(0, "HOGEHOGE")
-    return avoid(), 1000
+    if avoid() then
+      gcs:send_text(4, "Avoiding")
+      already_set = true
+    end
   end
-  if flight_mode == "GUIDED" and already_set == true then
-    already_set = false
-    gcs:send_text(0, "Rest Monitoring")
+  if flight_mode == 4 and already_set == true then
+    if return_orginal() then
+      already_set = false
+      gcs:send_text(0, "Rest Monitoring")
+    end
   end
   return update, 1000
 end

3b9626b906b3a4110be642c5f4b7c53023e35a6d
Katsutoshi Yotsuyanagi <kyotsuyanagi@cybermatrix.co>
2022-12-14 21:54:03 +0900

    update

--- lua/recever.lua
+++ lua/recever.lua
@@ -1,20 +1,17 @@
-already_set == false
+already_set = false
 
-function update () 
+function update() 
+  local original_location = ahrs:get_position()
   flight_mode = vehicle:get_mode()
   if flight_mode == 111 and already_set == false then
     gcs:send_text(0, "HOGEHOGE")
-    local target_vel = Vector3f()
-    target_vel:x(2)
-    vehicle:set_target_velocity_NED(target_vel))
+    vehicle:set_target_location(current:lat()-0.0001,current:lng(),current:alt())
     already_set = true
   end
   if flight_mode == 4 and already_set == true then
-    already_set = false
     gcs:send_text(0, "Rest Monitoring")
-    local target_vel = Vector3f()
-    target_vel:x(-2)
-    vehicle:set_target_velocity_NED(target_vel))
+    vehicle:set_target_location(current:lat()+0.0001,current:lng(),current:alt())
+    already_set = false
   end
   return update, 1000
 end
